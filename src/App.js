18f72c9fffa02baa0f4dfd29458c89732550856f
jahan-nusrat <njnusrat53@gmail.com>
2023-01-11 11:02:41 +0100

    styles setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+  return <h1 className="underline text-3xl">Hello World</h1>;
+};
 
-export default App
+export default App;
